b92bbe852b2c381ddb9251a9b6e257e525ab4632
c ds <carmine.disanti2@unibo.it>
2022-12-28 12:28:21 +0100

    Added API routes to the WS

--- README.md
+++ README.md
@@ -3,3 +3,102 @@
 Repository for development of the new (generalized) HADA version.
 
 For execution requirements.txt + CPLEX/docplex installation should suffice.
+
+## API usage
+
+### Get list of algorithms
+Route: `http://localhost:5000/algorithms`
+
+`GET` request.
+
+Response (example):
+```
+{
+  "algorithms": [
+    "correlation",
+    "fwt",
+    "convolution",
+    "mockalgo1",
+    "saxpy"
+  ]
+}
+```
+
+
+### Get informations about an algorithm
+Route: `http://localhost:5000/algorithms/<algorithm>`
+
+`GET` request.
+
+Response (example for "fwt" algorithm):
+```
+{
+  "algorithm": "fwt",
+  "hyperparameters": {
+    "var_0": {
+      "lb": 3,
+      "ub": 52
+    },
+    "var_1": {
+      "lb": 3,
+      "ub": 52
+    }
+  },
+  "targets": {
+    "memory": {
+      "lb": 24.450379,
+      "ub": 52.31878
+    },
+    "price": {
+      "lb": null,
+      "ub": null
+    },
+    "time": {
+      "lb": 60.57580995559693,
+      "ub": 864.9205942153931
+    }
+  }
+}
+```
+
+### Request an optimization (algorithm-specific)
+
+Route: `http://localhost:5000/optimize`
+
+`POST` request (application/json).
+
+Request (example):
+```
+curl -X POST -H 'Content-Type: application/json' -d 
+'{"algorithm":"correlation",
+  "objective": {"target":"memory", "type": "min"},
+   constraints: [
+    {'target': 'time', 'type': 'leq', value: 120},
+    ...
+   ],
+   prices: [
+    {'hw':'pc', price: 30},
+    ...
+   ]
+   }'
+http://localhost:5000/optimize
+```
+
+Response (example):
+```
+{
+  "solution": {
+    "hw": "vm",
+    "memory": 18.916514999999983,
+    "price": 30.0,
+    "time": 83.51820898,
+    "var_0": 3.0,
+    "var_1": 3.0,
+    "var_2": 3.0,
+    "var_3": 3.0,
+    "var_4": 3.0,
+    "var_5": 3.0,
+    "var_6": 10.0
+  }
+}
+```
--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from core.datasets import Datasets
 from core.ml_models import MLModels
 from core.optimization_request import OptimizationRequest, UserConstraints, HardwarePrices
 from core.hada import HADA
+
+
 # ==============================================================================
 # Service setup
 # ==============================================================================
-
-# Build the Flask app
 app = Flask(__name__)
 app.secret_key = ';u_QC&vzGaAR;&67vma[(4_cHZ;(F!;]dwjh&tJRBF;S(7aWYz/e=z!]^Fhk.K!@'
 
@@ -25,10 +25,7 @@ models = MLModels(db, 'algorithms/models')
 # ==============================================================================
 # Utility functions
 # ==============================================================================
-def run_hada(algorithm, form_dict):
-
-    # Merging form args and JSON configuration
-    optimization_request = parse_request(algorithm, form_dict)
+def run_hada(optimization_request):
 
     var_bounds = datasets.get_var_bounds_all(optimization_request)
     robust_coeff = datasets.get_robust_coeff(models, optimization_request)
@@ -36,7 +33,7 @@ def run_hada(algorithm, form_dict):
     solution = HADA(db, optimization_request, models, var_bounds, robust_coeff)
     return solution
 
-def parse_request(algorithm, form_dict):
+def parse_request_form(algorithm, form_dict):
 
     def sanitize_float(x):
         return None if x == '' else float(x)
@@ -63,6 +60,45 @@ def parse_request(algorithm, form_dict):
                                                 
     return optimization_request
 
+def parse_request_json(data):
+    '''
+    Example:
+    {
+        "algorithm":"fwt",
+        "robustness_fact": null,
+        "objective": {"target":"memory", "type": "min"},
+        "constraints": [
+            {'target': 'time', 'type': 'leq', value: 120},
+            ...
+        ],
+        prices: [
+            {'hw':'pc', price: 30},
+            ...
+        ]
+    }
+    '''
+    user_constraints = UserConstraints(db, data['algorithm'])
+    for constraint in data['constraints']:
+        user_constraints.add_constraint(constraint['target'],
+                                        constraint['type'],
+                                        constraint['value'])
+
+    hws_prices = HardwarePrices(db, data['algorithm'])
+    for hw_price in data['price_per_hw']:
+        hws_prices.add_hw_price(hw_price['hw'], hw_price['price'])
+
+
+    optimization_request = OptimizationRequest(db=db,
+                                               algorithm=data['algorithm'],
+                                               target=data['objective']['target'],
+                                               opt_type=data['objective']['type'],
+                                               robustness_fact=data['robustness_fact'],
+                                               user_constraints=user_constraints,
+                                               hws_prices=hws_prices)
+    return optimization_request
+
+
+
 def format_solution(solution):
     sol_hw = {'hw': solution.chosen_hw}
     sol_hyperparams = {hyperparam:val for hyperparam,val in solution.hyperparams_values.items()}
@@ -72,14 +108,13 @@ def format_solution(solution):
     return out
 
 # ==============================================================================
-# Routes
+# Routes (GUI)
 # ==============================================================================
-
 @app.route('/', methods=['GET', 'POST'])
 def hada_gui():
 
     out = None
-    if not session['last_selected_algo']:
+    if 'last_selected_algo' not in session:
         session['last_selected_algo'] = db.get_algorithms()[0]
     try:
         # two separate forms, one for algorithm selection and one for optimization requests
@@ -89,7 +124,8 @@ def hada_gui():
             if form_dict['form_id'] == 'select_algo':
                 session['last_selected_algo'] = form_dict['algorithm']
             if form_dict['form_id'] == 'optimize':
-                solution = run_hada(session['last_selected_algo'], form_dict)
+                optimization_request = parse_request_form(session['last_selected_algo'], form_dict)
+                solution = run_hada(optimization_request)
 
                 if solution:
                     out = format_solution(solution)
@@ -117,3 +153,62 @@ def hada_gui():
                            **rendering_kwargs,
                            selected_algo=session['last_selected_algo'],
                            out=out)
+
+# ==============================================================================
+# Routes (API)
+# ==============================================================================
+@app.route('/algorithms', methods=['GET'])
+def get_algorithms():
+    return jsonify({'algorithms': db.get_algorithms()})
+
+@app.route('/algorithms/<algorithm>', methods=['GET'])
+def get_algo_info(algorithm):
+
+    hyperparams = db.get_hyperparams(algorithm)
+    targets = db.get_targets(algorithm)
+    lb_per_var, ub_per_var = datasets.extract_var_bounds(algorithm)
+    lb_per_var['price'] = None
+    ub_per_var['price'] = None
+
+    
+    hyperparams_with_bounds = {hyperparam: {'lb': lb_per_var[hyperparam],
+                                            'ub': ub_per_var[hyperparam]}
+                               for hyperparam in hyperparams}
+
+    targets_with_bounds = {target: {'lb': lb_per_var[target],
+                                    'ub': ub_per_var[target]} 
+                           for target in targets}
+
+    hws_with_prices = {hw: {'default_price': price} 
+                       for hw,price in db.get_prices_per_hw(algorithm).items()}
+
+    ret = {'algorithm': algorithm,
+           'hws': hws_with_prices,
+           'hyperparameters': hyperparams_with_bounds,
+           'targets': targets_with_bounds}
+
+    # alternative
+    #ret = {'algorithm': algorithm,
+    #       'hyperparameters': hyperparams,
+    #       'targets': targets,
+    #       'bounds': {'lb_per_var': lb_per_var,
+    #                   'ub_per_var': ub_per_var}}
+
+    return jsonify(ret)
+
+@app.route('/optimize', methods=['POST'])
+def optimize():
+    data = request.get_json()
+    try:
+        optimization_request = parse_request_json(data)
+        solution = run_hada(optimization_request)
+
+        ret = {'solution': None}
+        if solution:
+            ret = {'solution': format_solution(solution)}
+
+    except Exception as e:
+        print(e)
+        ret = {'error': str(e)}
+
+    return jsonify(ret)
